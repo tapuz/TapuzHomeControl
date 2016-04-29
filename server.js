8a329a618a02527f3a9b05fc65af200aac0be9da
Michel Sieben <Michel Sieben>
2016-04-29 22:44:33 +0200

    kleine aanpassing om het server restart script te testen

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-var last_color = "blue";
+var last_color = "red";
 var connections;
 
 app.get('/', function(req, res){

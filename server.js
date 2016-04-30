663e42f86d3ddb622dbc01e3aa069c14b0037ba1
Michel Sieben <Michel Sieben>
2016-04-30 16:34:45 +0200

    de fade implementatie aan het proberen. alsook een paar esthetische aanpassingen

--- server.js
+++ server.js
@@ -13,49 +13,50 @@ app.get('/input', function(req, res){
 	});
 
 io.on('connection', function(socket){
- 	http.getConnections(function(error, count) {
- 	connections = count;
- 	console.log(connections + ' clients connected');
- 	}); 
-
-socket.on('color', function(color){
-	io.emit('color', color);
-	last_color = color;
-	console.log('new color is: ' + color);
-	});
- 
-socket.on('first_color', function (color){
-	io.emit('first_color', last_color);
-	});
-
-socket.on('fade',function(){
-	io.emit('mode','fade');
-   	console.log('fade');    
-	});
- 
-socket.on('strobe', function(mode){
-	switch (mode) {
-		case 'strobe_on':
-			io.emit('mode','strobe');
-			console.log('strobe_on');
-			break;
-		case 'strobe_off':
-			io.emit('mode','static');
-			console.log('strobe_off');
-			break;
-			}
-	}); 
- 
-io.emit('connections', connections);
-
-socket.on('disconnect', function(){
-	console.log('client disconnected');
+ 	
 	http.getConnections(function(error, count) {
 		connections = count;
 		console.log(connections + ' clients connected');
+		}); 
+	
+	socket.on('color', function(color){
+		io.emit('color', color);
+		last_color = color;
+		console.log('new color is: ' + color);
 		});
-	});
- 
+	 
+	socket.on('first_color', function (color){
+		io.emit('first_color', last_color);
+		});
+	
+	socket.on('fade',function(fade){
+		io.emit('fade',fade);
+		console.log(fade);    
+		});
+	 
+	socket.on('strobe', function(mode){
+		switch (mode) {
+			case 'strobe_on':
+				io.emit('mode','strobe');
+				console.log('strobe_on');
+				break;
+			case 'strobe_off':
+				io.emit('mode','static');
+				console.log('strobe_off');
+				break;
+				}
+		}); 
+	 
+	io.emit('connections', connections);
+	
+	socket.on('disconnect', function(){
+		console.log('client disconnected');
+		http.getConnections(function(error, count) {
+			connections = count;
+			console.log(connections + ' clients connected');
+			});
+		});
+	 
 });
 
 http.listen(3000, function(){

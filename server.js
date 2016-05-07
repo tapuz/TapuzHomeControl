a5031eddf68eb84bf90f982622f537709dfdd8cc
Thierry Duhameeuw <thierry.duhameeuw@gmail.com>
2016-05-07 08:51:01 +0000

    server.js edited online with Bitbucket

--- server.js
+++ server.js
@@ -1,4 +1,9 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
+
+
+
+//var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var last_color = "green";
